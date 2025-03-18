6343c14d86baf0da9f2744866479b2d0a7310c57
sshyta <sshyta@mail.ru>
2025-03-18 23:01:33 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:english_words/english_words.dart';
 
-void main (){
+void main() {
   runApp(MyApp());
 }
 
@@ -17,30 +17,59 @@ class MyApp extends StatelessWidget {
         title: 'Namer app',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange)
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 74, 34, 255),
+          ),
         ),
         home: MyHomePage(),
-      )
+      ),
     );
   }
 }
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+
+  void getNex() {
+    current = WordPair.random();
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
       body: Column(
         children: [
           Text('Param pam-pam'),
-          Text(appState.current.asLowerCase),
+          BigCard(pair: pair),
+
+          ElevatedButton(
+            onPressed: () {
+              appState.getNex();
+            },
+            child: Text('Next'),
+          ),
         ],
       ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({super.key, required this.pair});
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(pair.asLowerCase),
     );
   }
 }
